01eef79536329350e1090ffc92615c11def67633
RuanZzzz <734162396@qq.com>
2022-04-26 23:28:11 +0800

    synchronized的使用

--- case_java1/src/main/java/richard/test/Test17.java
+++ case_java1/src/main/java/richard/test/Test17.java
@@ -5,17 +5,22 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j(topic = "c.Test17")
 public class Test17 {
     static int counter = 0;
+    static Object lock = new Object();
 
     public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread(() -> {
             for (int i = 0; i < 5000; i++) {
-                counter ++;
+                synchronized (lock) {
+                    counter ++;
+                }
             }
         }, "t1");
 
         Thread t2 = new Thread(() -> {
             for (int i = 0; i < 5000; i++) {
-                counter --;
+                synchronized (lock) {
+                    counter --;
+                }
             }
         }, "t2");
 
