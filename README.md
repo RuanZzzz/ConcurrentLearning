3df360cf00ef0740fb7d2ac5c37c8f58ea7d6996
RuanZzzz <734162396@qq.com>
2022-04-27 21:57:58 +0800

    synchronized锁对象面向对象改进

--- README.md
+++ README.md
@@ -1274,3 +1274,55 @@ synchronized 实际是用 **对象锁** 保证了 **临界区内代码的原子�
 
 
 
+### 面向对象改进
+
+把需要保护的共享变量放入一个类
+
+```java
+public class Test17 {
+    public static void main(String[] args) throws InterruptedException {
+        Room room = new Room();
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 5000; i++) {
+                room.increment();
+            }
+        }, "t1");
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 5000; i++) {
+                room.decrement();
+            }
+        }, "t2");
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        log.debug("{}",room.getCounter());
+    }
+}
+
+class Room {
+    private int counter = 0;
+
+    public void increment() {
+        synchronized (this) {
+            counter ++;
+        }
+    }
+
+    public void decrement() {
+        synchronized (this) {
+            counter --;
+        }
+    }
+
+    public int getCounter() {
+        synchronized (this) {
+            return counter;
+        }
+    }
+}
+```
--- case_java1/src/main/java/richard/test/Test17.java
+++ case_java1/src/main/java/richard/test/Test17.java
@@ -4,23 +4,18 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j(topic = "c.Test17")
 public class Test17 {
-    static int counter = 0;
-    static Object lock = new Object();
-
     public static void main(String[] args) throws InterruptedException {
+        Room room = new Room();
+
         Thread t1 = new Thread(() -> {
             for (int i = 0; i < 5000; i++) {
-                synchronized (lock) {
-                    counter ++;
-                }
+                room.increment();
             }
         }, "t1");
 
         Thread t2 = new Thread(() -> {
             for (int i = 0; i < 5000; i++) {
-                synchronized (lock) {
-                    counter --;
-                }
+                room.decrement();
             }
         }, "t2");
 
@@ -29,6 +24,28 @@ public class Test17 {
         t1.join();
         t2.join();
 
-        log.debug("{}",counter);
+        log.debug("{}",room.getCounter());
+    }
+}
+
+class Room {
+    private int counter = 0;
+
+    public void increment() {
+        synchronized (this) {
+            counter ++;
+        }
+    }
+
+    public void decrement() {
+        synchronized (this) {
+            counter --;
+        }
+    }
+
+    public int getCounter() {
+        synchronized (this) {
+            return counter;
+        }
     }
 }
